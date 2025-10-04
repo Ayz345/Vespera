6c23416f121a3b168f98247662c82284d73fd7e1
Ayaz Büyükterzi <ayaz.buyukterzi.hza@gmail.com>
2025-10-04 11:03:13 +0300

    Kepler Distance Funciton added

--- Model1.py
+++ Model1.py
@@ -1,5 +1,23 @@
 import pandas as pd
-import numpy 
+import numpy as np
+def add_kepler_distance(df):
+    sigma = 5.670374419e-8
+    R_sun = 6.957e8
+    pc = 3.086e16
+    F0 = 3.6e-8
+    df = df.copy()
+    df["koi_srad"] = df["koi_srad"].astype(float).fillna(df["koi_srad"].median())
+    df["koi_steff"] = df["koi_steff"].astype(float).fillna(df["koi_steff"].median())
+    df["koi_kepmag"] = df["koi_kepmag"].astype(float).fillna(df["koi_kepmag"].median())
+    R_star = df["koi_srad"].values * R_sun
+    T_star = df["koi_steff"].values
+    m_kep = df["koi_kepmag"].values
+
+    L_star = 4 * np.pi * (R_star**2) * sigma * (T_star**4)
+    F_obs = F0 * 10**(-0.4 * m_kep)
+    d_m = np.sqrt(L_star / (4 * np.pi * F_obs))
+    df["koi_dist"] = d_m / pc
+    return df
 def model1(df,dataset_type):
         if dataset_type == "k2":
             label_col = "disposition"
@@ -10,10 +28,11 @@ def model1(df,dataset_type):
             neg_label = "FALSE POSITIVE"
             candidate_label = "CANDIDATE"
         elif dataset_type == "kepler":
+            df=add_kepler_distance(df)
             label_col = "koi_disposition"
             id_col = "kepid"
             feature_cols = ['koi_period','koi_impact','koi_duration','koi_depth','koi_prad','koi_teq','koi_slogg','koi_srad']
-            Position = ['ra','dec']
+            Position = ['ra','dec','koi_dist']
             pos_label = "CONFIRMED"
             neg_label = "FALSE POSITIVE"
             candidate_label = "CANDIDATE"
