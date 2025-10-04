c19a1bc70d1fa7a2ca660bb749bb2b87b4fab0cd
Ayaz Büyükterzi <ayaz.buyukterzi.hza@gmail.com>
2025-10-04 11:05:53 +0300

    Slight changes in names

--- Model1.py
+++ Model1.py
@@ -23,7 +23,7 @@ def model1(df,dataset_type):
             label_col = "disposition"
             id_col = "pl_name"
             feature_cols = ['pl_orbper','sy_dist','sy_vmag','sy_kmag','sy_gaiamag']
-            Position = ['ra','dec','sy_dist']
+            position_cols = ['ra','dec','sy_dist']
             pos_label = "CONFIRMED"
             neg_label = "FALSE POSITIVE"
             candidate_label = "CANDIDATE"
@@ -32,7 +32,7 @@ def model1(df,dataset_type):
             label_col = "koi_disposition"
             id_col = "kepid"
             feature_cols = ['koi_period','koi_impact','koi_duration','koi_depth','koi_prad','koi_teq','koi_slogg','koi_srad']
-            Position = ['ra','dec','koi_dist']
+            position_cols = ['ra','dec','koi_dist']
             pos_label = "CONFIRMED"
             neg_label = "FALSE POSITIVE"
             candidate_label = "CANDIDATE"
@@ -40,7 +40,7 @@ def model1(df,dataset_type):
             label_col = "tfopwg_disp"
             id_col = "toi"
             feature_cols = ['st_pmra','pl_trandurh','pl_trandep','pl_rade','pl_insol','pl_eqt','st_tmag','st_dist','st_teff']
-            Position = ['ra','dec','st_dist']   
+            position_cols = ['ra','dec','st_dist']   
             pos_label = ["CP","KP"]
             neg_label = ["FP"]
             candidate_label = ["PC","APC"]
